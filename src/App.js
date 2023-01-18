d82cfa169d7062e350513de9a099b51d54c236ea
Oscar francisco Lopez escobar <olopeze@outlook.com>
2023-01-17 20:13:11 -0600

    added routing

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import { Navbar } from './components';
-
+import {
+  About,
+  Contact,
+  Exhibitions,
+  Home,
+  NotFound,
+  Work
+} from './views';
+ 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <MemoryRouter>
+
         <Routes>
-          <Routes path="/"/>
-          <Routes path="/exhibitions" />
-          <Routes />
-          <Routes />
-          <Routes />
+          <Route path="/" element={<Home />} />
+          <Route path="/exhibitions" element={<Exhibitions />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/about-me" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
-      </MemoryRouter>
+
     </div>
   );
 }
---
+++ src/views/index.js
@@ -0,0 +1,15 @@
+import About from './about';
+import Contact from './contact';
+import Exhibitions from './exhibitions';
+import Home from './home';
+import NotFound from './notFound';
+import Work from './work';
+
+export {
+  About,
+  Contact,
+  Exhibitions,
+  Home,
+  NotFound,
+  Work
+}
